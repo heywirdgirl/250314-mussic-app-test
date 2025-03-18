a94c2d803599ce5b5130065d5fa1cbce77daffe2
heywirdgirl <awsmydream@gmail.com>
2025-03-18 22:57:34 +0700

    1057

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
     <ThemeProvider theme={theme}>
       <CssBaseline /> {/* Normalize default styles */}
       <Header />
-      <Container maxWidth="lg" sx={{ minHeight: "80vh", py: 3 }}>
+      <Container maxWidth="lg" sx={{ minHeight: "80vh", py: 3 ,px:}}>
         <Component {...pageProps} />
       </Container>
       <Footer />
